5be1998b53cad0cdcf48d2d321a980c049530189
Rishabh Saini <61822388+RishabhSaini619@users.noreply.github.com>
2023-08-09 16:11:16 +0530

    update meta tag icon

--- src/App.jsx
+++ src/App.jsx
@@ -3,34 +3,47 @@ import Header from "./Components/Header/Header.jsx";
 import Schools from "./Components/Schools/Schools";
 import StudentPage from "./Components/Students/Students";
 import "./App.css";
-import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
-
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 
 const App = () => {
   const [selectedSchool, setSelectedSchool] = useState([]);
 
   return (
     <Router>
-    <div>
-      <Switch>
-        <Route exact path="/">
-          <main className="App">
-            <Header />
-            {!selectedSchool.length ? (
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <main className="App">
+              <Header />
+              {!selectedSchool.length ? (
+                <Schools
+                  selectedSchool={selectedSchool}
+                  setSelectedSchool={setSelectedSchool}
+                />
+              ) : (
+                <StudentPage selectedSchool={selectedSchool} />
+              )}
+            </main>
+          </Route>
+        </Switch>
+      </div>
+      {/* <Router>
+        <main>
+          <Header />
+          <Switch>
+            <Route exact path="/">
               <Schools
                 selectedSchool={selectedSchool}
                 setSelectedSchool={setSelectedSchool}
               />
-            ) : (
-              <StudentPage 
-                selectedSchool={selectedSchool}
-              />
-            )}
-          </main>
-        </Route>
-      </Switch>
-    </div>
-  </Router>
+            </Route>
+            <Route exact path="/Students">
+              <StudentPage selectedSchool={selectedSchool} />
+            </Route>
+          </Switch>
+        </main>
+      </Router> */}
+    </Router>
   );
 };
 
--- src/Components/Schools/Schools.jsx
+++ src/Components/Schools/Schools.jsx
@@ -5,12 +5,23 @@ import { useLocation } from "react-router-dom";
 
 function useQuery() {
   const { search } = useLocation();
-
   return React.useMemo(() => new URLSearchParams(search), [search]);
 }
 
 const Schools = ({ selectedSchool, setSelectedSchool }) => {
   const [schools, setSchools] = useState([]);
+//   const [schoolQuery, setSchoolQuery] = useState([]);
+//   const [studentQuery, setStudentQuery] = useState([]);
+
+//   const handleSchoolQueryChange = (event) => {
+//     setSchoolQuery(event.target.value);
+//   };
+//   const handleStudentQueryChange = (event) => {
+//     setStudentQuery(event.target.value);
+//  };
+
+//   const handleSearchClick = () => {
+//   };
 
   const query = useQuery();
 
@@ -24,52 +35,74 @@ const Schools = ({ selectedSchool, setSelectedSchool }) => {
   const handleSchoolClick = async (jobId) => {
     console.log("handleSchoolClick", selectedSchool);
     getOrderByJobId(jobId)
-      .then((response) => 
-        (query.get('id')) ? setSelectedSchool(response.data.filter((item) => item.receiver_details.name.includes(query.get('id')))) : setSelectedSchool(response.data)
+      .then((response) =>
+        query.get("id")
+          ? setSelectedSchool(
+              response.data.filter((item) =>
+                item.receiver_details.name.includes(query.get("id"))
+              )
+            )
+          : setSelectedSchool(response.data)
       )
       .catch((error) => console.error(error));
   };
 
   useEffect(() => {
-    handleSchoolClick(query.get('schooldId'));
-  }, [query.get('schooldId')]);
+    handleSchoolClick(query.get("schooldId"));
+  }, [query.get("schooldId")]);
 
   return (
     <main className="School">
-      <div className="schoolHeader">
-        <div className="Heading">
-          Select your school</div>
-          <div className="search">
-            <div className="searchcontent">
-              <div className="searchBar">search bar</div>
-              <div className="searchBtn">search</div>
+      <header className="Heading">Select your school</header>
+      <div className="School_Content">
+        {schools
+          .map((school) => (
+            <div
+              className="Card"
+              key={school._id}
+              onClick={() => {
+                handleSchoolClick(school._id);
+              }}
+            >
+              <img
+                className="Image"
+                src={school.display_logo_url}
+                alt="Job_Image"
+              />
+              <div className="Label">{school.display_school_name}</div>
             </div>
-            <div className="searchStudent">
-            <div className="searchBar">search bar</div>
-              <div className="searchBtn">search</div>
-            </div>
-          </div>
-          </div>
-        <div className="School_Content">
-          {schools
-            .map((school) => (
-              <div
-                className="Card"
-                key={school._id}
-                onClick={() => {
-                  handleSchoolClick(school._id);
-                }}
-              >
-                <img
-                  className="Image"
-                  src={school.display_logo_url}
-                  alt="Job_Image"
-                />
-                <div className="Label">{school.display_school_name}</div>
-              </div>
-            ))
-            .sort()}
+          ))
+          .sort()}
       </div>
+      {/* <div className="search">
+        <div className="searchInputs">
+          <div className="searchSchhool">
+            <div className="Label">School Name</div>
+            <input
+              type="text"
+              value={schoolQuery}
+              onChange={handleSchoolQueryChange}
+              className="searchBar"
+              id="searchSchool"
+            />
+          </div>
+          <div className="searchStudent">
+            <div className="Label">Receiver Name</div>
+            <input
+              type="text"
+              value={studentQuery}
+              onChange={handleStudentQueryChange}
+              className="searchBar"
+              id="searchStudent"
+              variant="outlined"
+              label="Student Name"
+            />
+          </div>
+        </div>
+        <button className="searchBtn" onClick={handleSearchClick}>
+          <div className="Label">Search</div>
+        </button>
+      </div> */}
     </main>
   );
 };
